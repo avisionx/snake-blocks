dbbdef0ce20a1689f74b84da6104374d83ed5767
Avi Garg <32339251+avisionx@users.noreply.github.com>
2018-10-31 18:51:40 +0530

    Saving my code

--- src/application/Ball.java
+++ src/application/Ball.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 public class Ball extends GameObject{
 
 	public Ball() {
-		super(new circleWithText(200, 10, 10, Color.BISQUE, 10), 10);
+		super(new circleWithText(200, 0, 15, Color.BISQUE, 10), 5);
 	}
 	
 }
--- src/application/Snake.java
+++ src/application/Snake.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.scene.Group;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -20,23 +21,52 @@ class circleWithText extends StackPane {
 	
 }
 
+class snakeBody extends Group{
+	
+	public snakeBody(int x, int y, int radius, Color color, int length) {
+		super();
+		this.getChildren().add(new Circle(0, 0, radius, color));
+		for (int i = 1; i < Math.min(length, 7); i++) {
+			this.getChildren().add(new Circle(0, 2*i*radius, radius, color));
+		}
+		this.setTranslateX(x + radius);
+		this.setTranslateY(y + radius);
+	}
+	
+	public void moveHead(double oldX, double newX) {
+		System.out.println(newX);
+		this.setTranslateX(newX);
+	}
+}
+
 public class Snake extends GameObject{
 
 	private int length;
 	private double xCord;
 	private double yCord;
+	public double xVelocity = 0;
+	public double speed = 300;
+	public static double minVelocity = 0;
+	public static double maxVelocity = 600;
 	
 	public Snake(int length) {
-		
-		super(new circleWithText(Main.getScenewidth()/2, Main.getSceneheight()*9/10, 10, Color.AQUA, length));
+		super(new snakeBody(Main.getScenewidth()/2, Main.getSceneheight()*8/10, 15, Color.AQUA, length));
 		this.length = length;
 		this.xCord = this.getView().getTranslateX();
 		this.yCord = this.getView().getTranslateY();
 		
 	}
+	
+	public void moveLeft() {
+		this.getView().setTranslateX(this.getView().getTranslateX() - 10);
+	}
+	
+	public void moveRight() {
+		this.getView().setTranslateX(this.getView().getTranslateX() + 10);
+	}
 
 	public void update() {
 		return;
-	}
+	}	
 	
 }
