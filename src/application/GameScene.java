132b912de06555578e8ff0f721b041b358983014
Avi Garg <32339251+avisionx@users.noreply.github.com>
2018-11-17 01:35:42 +0530

    save

--- src/application/GameScene.java
+++ src/application/GameScene.java
@@ -24,7 +24,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-class GamePauseHandler implements EventHandler<ActionEvent>{
+class GamePauseHandler implements EventHandler<ActionEvent> {
 
 	@Override
 	public void handle(ActionEvent e) {
@@ -34,10 +34,10 @@ class GamePauseHandler implements EventHandler<ActionEvent>{
 	public void handle(KeyEvent e) {
 		GameScene.pauseGame();
 	}
-	
+
 }
 
-class GameResumeHandler implements EventHandler<ActionEvent>{
+class GameResumeHandler implements EventHandler<ActionEvent> {
 
 	@Override
 	public void handle(ActionEvent e) {
@@ -47,29 +47,29 @@ class GameResumeHandler implements EventHandler<ActionEvent>{
 	public void handle(KeyEvent e) {
 		GameScene.resumeGame();
 	}
-	
+
 }
 
-class pauseScreenButton extends Button{
-	
-	public pauseScreenButton(String name){
+class pauseScreenButton extends Button {
+
+	public pauseScreenButton(String name) {
 		super(name);
 		this.getStyleClass().add("pasueScreenBtn");
 	}
-	
+
 }
 
 class pauseScreen extends StackPane {
-	
+
 	public pauseScreen(int scoreToDisplay) {
-		
+
 		super();
-		
+
 		Rectangle background = new Rectangle(0, 0, Main.getScenewidth(), Main.getSceneheight());
 		background.setFill(Color.rgb(0, 0, 0, 0.5));
-		
+
 		VBox displayItems = new VBox(30);
-		
+
 		HBox scoreWithCrown = new HBox(10);
 		Text scoreText = new Text(scoreToDisplay + "");
 		scoreText.getStyleClass().add("pauseScoreText");
@@ -82,60 +82,58 @@ class pauseScreen extends StackPane {
 			System.out.println(e.getMessage());
 		}
 		ImageView crownImageView = null;
-		if(corwnImage != null) {
+		if (corwnImage != null) {
 			crownImageView = new ImageView(corwnImage);
 			crownImageView.setX(0);
 			crownImageView.setY(0);
 			crownImageView.setFitWidth(45);
-			crownImageView.setPreserveRatio(true);  
+			crownImageView.setPreserveRatio(true);
 		}
 		scoreWithCrown.setAlignment(Pos.CENTER);
 		scoreWithCrown.getChildren().addAll(crownImageView, scoreText);
-		
+
 		Button resumeBtn = new pauseScreenButton("Resume");
 		Button newGameBtn = new pauseScreenButton("New Game");
 		Button goToMenuBtn = new pauseScreenButton("Main Menu");
 		Button exitBtn = new pauseScreenButton("Exit");
-		
+
 		resumeBtn.setOnAction(e -> {
 			GameScene.gameResumeHandler.handle(e);
 		});
-		
+
 		newGameBtn.setOnAction(e -> {
 			GameScene gameScene = new GameScene(GameScene.primaryStage, GameScene.mainMenuScene);
 			GameScene.primaryStage.setScene(gameScene);
-			gameScene.getStylesheets().add(
-	        		getClass().getResource("application.css").toExternalForm()
-	        	);
+			gameScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		});
-		
+
 		goToMenuBtn.setOnAction(e -> {
 			GameScene.primaryStage.setScene(GameScene.mainMenuScene);
 		});
-		
+
 		exitBtn.setOnAction(e -> {
 			System.exit(0);
 		});
-		
+
 		displayItems.getChildren().addAll(scoreWithCrown, resumeBtn, newGameBtn, goToMenuBtn, exitBtn);
 		displayItems.setAlignment(Pos.CENTER);
 		this.getChildren().addAll(background, displayItems);
-	
-	}	
-	
+
+	}
+
 }
 
 class endScreen extends StackPane {
-	
+
 	public endScreen(int scoreToDisplay) {
-		
+
 		super();
-		
+
 		Rectangle background = new Rectangle(0, 0, Main.getScenewidth(), Main.getSceneheight());
 		background.setFill(Color.rgb(0, 0, 0, 0.5));
-		
+
 		VBox displayItems = new VBox(30);
-		
+
 		HBox scoreWithCrown = new HBox(10);
 		Text scoreText = new Text(scoreToDisplay + "");
 		scoreText.getStyleClass().add("pauseScoreText");
@@ -148,52 +146,50 @@ class endScreen extends StackPane {
 			System.out.println(e.getMessage());
 		}
 		ImageView crownImageView = null;
-		if(corwnImage != null) {
+		if (corwnImage != null) {
 			crownImageView = new ImageView(corwnImage);
 			crownImageView.setX(0);
 			crownImageView.setY(0);
 			crownImageView.setFitWidth(45);
-			crownImageView.setPreserveRatio(true);  
+			crownImageView.setPreserveRatio(true);
 		}
 		scoreWithCrown.setAlignment(Pos.CENTER);
 		scoreWithCrown.getChildren().addAll(crownImageView, scoreText);
-		
+
 		Button newGameBtn = new pauseScreenButton("New Game");
 		Button goToMenuBtn = new pauseScreenButton("Main Menu");
 		Button exitBtn = new pauseScreenButton("Exit");
-		
+
 		newGameBtn.setOnAction(e -> {
 			GameScene gameScene = new GameScene(GameScene.primaryStage, GameScene.mainMenuScene);
 			GameScene.primaryStage.setScene(gameScene);
-			gameScene.getStylesheets().add(
-	        		getClass().getResource("application.css").toExternalForm()
-	        	);
+			gameScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		});
-		
+
 		goToMenuBtn.setOnAction(e -> {
 			GameScene.primaryStage.setScene(GameScene.mainMenuScene);
 		});
-		
+
 		exitBtn.setOnAction(e -> {
 			System.exit(0);
 		});
-		
+
 		displayItems.getChildren().addAll(scoreWithCrown, newGameBtn, goToMenuBtn, exitBtn);
 		displayItems.setAlignment(Pos.CENTER);
 		this.getChildren().addAll(background, displayItems);
-	
-	}	
-	
+
+	}
+
 }
 
 public class GameScene extends Scene {
-	
+
 	private static Pane root;
 	private static Snake userSnake;
 	private static List<GameObject> tokens = new ArrayList<>();
 	private static List<Block> blocks = new ArrayList<>();
 	private static List<Wall> walls = new ArrayList<>();
-	private static double gameSpeed;	
+	private static double gameSpeed;
 	private static Button pauseButton;
 	private static GamePauseHandler gamePauseHandler;
 	protected static GameResumeHandler gameResumeHandler;
@@ -205,75 +201,72 @@ public class GameScene extends Scene {
 	private static boolean openedPauseMenu = false;
 	protected static Stage primaryStage;
 	protected static Scene mainMenuScene;
-	
-	
+
 	private static int interactablesCount = 0;
-	
+
 	private static AnimationTimer mainFrameTimer = new AnimationTimer() {
-		
+
 		@Override
 		public void handle(long now) {
 			updateEachFrame();
 		}
-		
+
 	};
-	
+
 	private static AnimationTimer populationTimer = new AnimationTimer() {
-		
+
 		double lastPopulateTime = System.currentTimeMillis();
-		
+
 		@Override
 		public void handle(long now) {
-			
-			double elapsedTimeInSec = (now-lastPopulateTime)/1_000_000_000.0 ;
-			if(elapsedTimeInSec >= 1) {
+
+			double elapsedTimeInSec = (now - lastPopulateTime) / 1_000_000_000.0;
+			if (elapsedTimeInSec >= 1) {
 				populateNewItems();
 				lastPopulateTime = now;
 			}
 		}
-		
+
 	};
-	
+
 	public static void setGameScore(int newScore) {
 		curGameScore = newScore;
 		scoreOnGame.setText(newScore + "");
 	}
-	
+
 	public static int getGameScore() {
 		return curGameScore;
 	}
-	
-	public GameScene(Stage stage, Scene mainScreen) {
-		
-		super(new Pane(createContent()), Main.getScenewidth(), Main.getSceneheight());	
-		
-		this.setOnKeyPressed(e -> {	
 
-			if(e.getCode() == KeyCode.A) {
+	public GameScene(Stage stage, Scene mainScreen) {
+
+		super(new Pane(createContent()), Main.getScenewidth(), Main.getSceneheight());
+
+		this.setOnKeyPressed(e -> {
+
+			if (e.getCode() == KeyCode.A) {
 				userSnake.moveLeft();
-			}
-			else if(e.getCode() == KeyCode.D) {
+			} else if (e.getCode() == KeyCode.D) {
 				userSnake.moveRight();
-			}
-			else if(e.getCode() == KeyCode.P) {
-				if(!openedPauseMenu)
+			} else if (e.getCode() == KeyCode.P) {
+				if (!openedPauseMenu)
 					gamePauseHandler.handle(e);
 				else
 					gameResumeHandler.handle(e);
 			}
 		});
-		
-		this.setOnKeyReleased(e -> {	
-			if(e.getCode() == KeyCode.A || e.getCode() == KeyCode.D) {
+
+		this.setOnKeyReleased(e -> {
+			if (e.getCode() == KeyCode.A || e.getCode() == KeyCode.D) {
 				userSnake.stopSnake();
 			}
 		});
-		
+
 		primaryStage = stage;
 		mainMenuScene = mainScreen;
-		
+
 	}
-	
+
 	protected static void pauseGame() {
 		openedPauseMenu = true;
 		stopFallAnimation();
@@ -282,7 +275,7 @@ public class GameScene extends Scene {
 		pausedMenu = new pauseScreen(curGameScore);
 		root.getChildren().add(pausedMenu);
 	}
-	
+
 	private static void stopFallAnimation() {
 		for (GameObject token : tokens) {
 			token.getFallDownTimer().stop();
@@ -294,17 +287,17 @@ public class GameScene extends Scene {
 			block.getFallDownTimer().stop();
 		}
 	}
-	
+
 	protected static void resumeGame() {
-		
+
 		openedPauseMenu = false;
 		resumeFallAnimation();
 		populationTimer.start();
 		userSnake.setSpeed(400);
 		root.getChildren().remove(pausedMenu);
-	
+
 	}
-	
+
 	private static void resumeFallAnimation() {
 		for (GameObject token : tokens) {
 			token.getFallDownTimer().start();
@@ -318,69 +311,69 @@ public class GameScene extends Scene {
 	}
 
 	private static Parent createContent() {
-		
+
 		resetGame();
 		root = new Pane();
 		root.setPrefSize(Main.getScenewidth(), Main.getSceneheight());
 		root.getStyleClass().add("rootBg");
-		
+
 		scoreOnGameBox = new HBox(7);
 		scoreOnGame = new Text(curGameScore + "");
-		
+
 		scoreOnGame.getStyleClass().add("scoreGameText");
 		scoreOnGame.setFill(Color.WHITE);
-		
+
 		Image corwnImage = null;
-		
+
 		try {
 			String pathToImage = "./img/scoreCrown.png";
 			corwnImage = new Image(new FileInputStream(pathToImage));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		ImageView crownImageView = null;
-		if(corwnImage != null) {
+		if (corwnImage != null) {
 			crownImageView = new ImageView(corwnImage);
 			crownImageView.setX(0);
 			crownImageView.setY(0);
 			crownImageView.setFitWidth(20);
-			crownImageView.setPreserveRatio(true);  
+			crownImageView.setPreserveRatio(true);
 		}
-		
+
 		scoreOnGameBox.setAlignment(Pos.CENTER);
 		scoreOnGameBox.getChildren().addAll(crownImageView, scoreOnGame);
-		
+
 		scoreOnGameBox.setTranslateX(10);
 		scoreOnGameBox.setTranslateY(10);
-		
+
 		pauseButton = new Button();
 		pauseButton.getStyleClass().add("pauseBtn");
 		pauseButton.setTranslateX(Main.getScenewidth() - 40);
 		pauseButton.setTranslateY(Main.getSceneheight() - 40);
-		
+
 		gamePauseHandler = new GamePauseHandler();
 		pauseButton.setOnAction(gamePauseHandler);
-		
+
 		gameResumeHandler = new GameResumeHandler();
-		
+
 		root.getChildren().addAll(scoreOnGameBox, pauseButton);
 
-		userSnake =  new Snake(10);
+		userSnake = new Snake(10);
 		addSnake(userSnake);
-		
+
 		mainFrameTimer.start();
 		populationTimer.start();
-		
+
 		return root;
-		
+
 	}
-	
+
 	public static void gameOver() {
 		System.out.println("GAME OVER");
 		endGame();
 	}
-	
+
 	private static void endGame() {
 		stopFallAnimation();
 		populationTimer.stop();
@@ -388,13 +381,13 @@ public class GameScene extends Scene {
 		endMenu = new endScreen(curGameScore);
 		root.getChildren().add(endMenu);
 	}
-	
+
 	private static void resetGame() {
 		userSnake = null;
 		tokens = new ArrayList<>();
 		blocks = new ArrayList<>();
 		walls = new ArrayList<>();
-		gameSpeed = 4.5;	
+		gameSpeed = 4.5;
 		curGameScore = 0;
 		openedPauseMenu = false;
 	}
@@ -404,131 +397,134 @@ public class GameScene extends Scene {
 		pauseButton.toFront();
 		scoreOnGameBox.toFront();
 	}
-	
+
 	private static void addGameObject(GameObject object) {
 		root.getChildren().add(object.getView());
 		userSnake.toFront();
 		pauseButton.toFront();
 		scoreOnGameBox.toFront();
 	}
-	
+
 	private static void updateEachFrame() {
-		
-		for(GameObject token : tokens) {
-			
-			if(token.isColliding(userSnake.getSnakeHead())) {
+
+		for (GameObject token : tokens) {
+
+			if (token.isColliding(userSnake.getSnakeHead())) {
 				token.setAlive(false);
 				((Interactable) token).collide(userSnake);
 				root.getChildren().remove(token.getView());
 			}
-			if(token.isDead()) {
+			if (token.isDead()) {
 				root.getChildren().remove(token.getView());
 			}
-			
+
 		}
-		
-		for(Block block : blocks) {
-			if(block.isColliding(userSnake.getSnakeHead())) {
-				Point2D rectPos = ((rectangleWithText)block.getView()).getRectCenter();
+
+		for (Block block : blocks) {
+			if (block.isColliding(userSnake.getSnakeHead())) {
+				Point2D rectPos = ((rectangleWithText) block.getView()).getRectCenter();
 				Point2D snakePos = userSnake.getSnakeHeadPosPoint2D();
 				Point2D trianglePoint = new Point2D(rectPos.getX(), snakePos.getY());
-				if(Math.atan(snakePos.distance(trianglePoint)/trianglePoint.distance(rectPos)) > 0.77 || snakePos.getY() <= rectPos.getY()) {
+				if (Math.atan(snakePos.distance(trianglePoint) / trianglePoint.distance(rectPos)) > 0.77
+						|| snakePos.getY() <= rectPos.getY()) {
 					userSnake.setSnakeCollideBlock(true);
-				}
-				else {
+				} else {
 					block.setAlive(false);
 					block.collide(userSnake);
 					root.getChildren().remove(block.getView());
 				}
 			}
-			if(block.isDead()) {
+			if (block.isDead()) {
 				root.getChildren().remove(block.getView());
 			}
 		}
-		
-		for(Wall wall : walls) {
-			if(wall.isDead()) {
+
+		for (Wall wall : walls) {
+			if (wall.isDead()) {
 				root.getChildren().remove(wall.getView());
 			}
 		}
-		
+
 		tokens.removeIf(GameObject::isDead);
 		blocks.removeIf(GameObject::isDead);
 		walls.removeIf(GameObject::isDead);
-		
+
 	}
 
-	static GameObject collideWall(){
-		for(GameObject wall : walls){
-			if(wall.isColliding(userSnake.getSnakeHead())) {
+	static GameObject collideWall() {
+		for (GameObject wall : walls) {
+			if (wall.isColliding(userSnake.getSnakeHead())) {
 				return wall;
 			}
 		}
 		return null;
 	}
-	
+
 	public static void populateNewItems() {
 
-		Ball newBall = new Ball(1 + Math.random()*(Main.getScenewidth() - 30), -50, (int)(1 + Math.floor(Math.random()*20)), gameSpeed);
-		DestroyBlocks newDestroyBlock = new DestroyBlocks(20 + Math.random()*(Main.getScenewidth() - 20), -40, gameSpeed);
-		Magnet newMagnet = new Magnet(20 + Math.random()*(Main.getScenewidth() - 20), -40, gameSpeed);
-		Shield newShield = new Shield(20 + Math.random()*(Main.getScenewidth() - 20), -40, gameSpeed);
+		Ball newBall = new Ball(1 + Math.random() * (Main.getScenewidth() - 30), -50,
+				(int) (1 + Math.floor(Math.random() * 20)), gameSpeed);
+		DestroyBlocks newDestroyBlock = new DestroyBlocks(20 + Math.random() * (Main.getScenewidth() - 20), -40,
+				gameSpeed);
+		Magnet newMagnet = new Magnet(20 + Math.random() * (Main.getScenewidth() - 20), -40, gameSpeed);
+		Shield newShield = new Shield(20 + Math.random() * (Main.getScenewidth() - 20), -40, gameSpeed);
 
-		if(isSafe(newBall)){
-			tokens.add(newBall); 
-			addGameObject(newBall); 
+		if (isSafe(newBall)) {
+			tokens.add(newBall);
+			addGameObject(newBall);
 			interactablesCount++;
 		}
-		
-		if(isSafe(newMagnet) && interactablesCount%4 == 0){
-			tokens.add(newMagnet); 
-			addGameObject(newMagnet); 
-		}
-		
-		if(isSafe(newDestroyBlock) && interactablesCount%5 == 0){
-			tokens.add(newDestroyBlock); 
-			addGameObject(newDestroyBlock); 
-		}
-		
-		if(isSafe(newShield) && interactablesCount%4 == 0){
-			tokens.add(newShield); 
-			addGameObject(newShield); 
+
+		if (isSafe(newMagnet) && interactablesCount % 4 == 0) {
+			tokens.add(newMagnet);
+			addGameObject(newMagnet);
 		}
 
-		Block newBlock = new Block(1 + Math.random()*(Main.getScenewidth() - 62), -61, (int)(1 + Math.floor(Math.random()*56)), gameSpeed);
-		if(isSafe(newBlock)) {
-			blocks.add(newBlock); 
+		if (isSafe(newDestroyBlock) && interactablesCount % 5 == 0) {
+			tokens.add(newDestroyBlock);
+			addGameObject(newDestroyBlock);
+		}
+
+		if (isSafe(newShield) && interactablesCount % 4 == 0) {
+			tokens.add(newShield);
+			addGameObject(newShield);
+		}
+
+		Block newBlock = new Block(1 + Math.random() * (Main.getScenewidth() - 62), -61,
+				(int) (1 + Math.floor(Math.random() * 56)), gameSpeed);
+		if (isSafe(newBlock)) {
+			blocks.add(newBlock);
 			addGameObject(newBlock);
 		}
-		
-		Wall newWall = new Wall(1 + Math.random()*(Main.getScenewidth() - 6), 80 + Math.random()*200, gameSpeed);
-		if(isSafe(newWall)){ 
-			walls.add(newWall); 
-			addGameObject(newWall); 
+
+		Wall newWall = new Wall(1 + Math.random() * (Main.getScenewidth() - 6), 80 + Math.random() * 200, gameSpeed);
+		if (isSafe(newWall)) {
+			walls.add(newWall);
+			addGameObject(newWall);
 		}
-		
+
 	}
 
-	static boolean isSafe(GameObject object){
-		
-		for(GameObject token : tokens){
-			if(token.isColliding(object)){
+	static boolean isSafe(GameObject object) {
+
+		for (GameObject token : tokens) {
+			if (token.isColliding(object)) {
 				return false;
 			}
 		}
-		for(GameObject wall : walls){
-			if(wall.isColliding(object)){
+		for (GameObject wall : walls) {
+			if (wall.isColliding(object)) {
 				return false;
 			}
 		}
-		for(GameObject block : blocks){
-			if(block.isColliding(object)){
+		for (GameObject block : blocks) {
+			if (block.isColliding(object)) {
 				return false;
 			}
 		}
-		
+
 		return true;
-	
+
 	}
-	
+
 }
