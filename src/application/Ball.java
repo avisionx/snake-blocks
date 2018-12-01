2f5430a02a58808b4d4359282049ce7ef5df5711
Avi Garg <32339251+avisionx@users.noreply.github.com>
2018-12-02 00:46:15 +0530

    Ball, Token, Css

--- src/application/Ball.java
+++ src/application/Ball.java
@@ -15,39 +15,42 @@ class circleWithText extends VBox {
 	Circle circleBody;
 	
 	public circleWithText(double x, double y, double radius, Color color, int value) {
+		
 		super();
+		
 		this.circleBody = new Circle(x, y, radius, color);
 		Text circleText = new Text(value + "");
+		
 		circleText.setFill(Color.WHITE);
 		circleText.getStyleClass().add("circleFont");
 		circleText .setBoundsType(TextBoundsType.VISUAL); 
+		
 		this.getChildren().addAll(circleText, this.circleBody);
-		this.setSpacing(5);
+		
 		this.setAlignment(Pos.CENTER);
+		this.setSpacing(5);
 		this.setTranslateX(x);
 		this.setTranslateY(y);
+	
 	}
 	
 }
 
-public class Ball extends GameObject implements Token{
+public class Ball extends GameObject implements Token { 
 
 	private int value;
 	
 	public Ball(double x, double y, int value, double speed) {
-		
 		super(new circleWithText(x, y, 13, Color.web("#ffdd0c"), value), speed);
 		this.value = value;
 		this.getFallDownTimer().start();
-		
 	}
 
 	@Override
 	public void collide(Snake snake) {
 		snake.setSnakeLength(snake.getSnakeLength() + this.value);
 		ParticleBurst burstAnimation = new ParticleBurst(this.getView().getTranslateX(), this.getView().getTranslateY(), Color.web("#ffdd0c"));
-		GameScene.root.getChildren().add(burstAnimation);
-		
+		GameScene.root.getChildren().add(burstAnimation);	
 	}
 
 	public void attract(Point2D snakePos) {
@@ -61,6 +64,7 @@ public class Ball extends GameObject implements Token{
 
 				Node view = getView();
 				Point2D newPosVector = snakePos.subtract(getPos2D()).normalize().multiply(2);
+				
 				view.setTranslateX(view.getTranslateX() + newPosVector.getX());
 				view.setTranslateY(view.getTranslateY() + newPosVector.getY());
 				
